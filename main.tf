c2620b229729cd46ee29d4360a11999bde61f072
Khaleel94 <khaleelullah021@gmail.com>
2025-06-02 16:30:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-024790dfd513f7950"
 instance_type = "t2.medium"
-key_name = "swikp"
+key_name = "Netflix"
 vpc_security_group_ids = ["sgr-03b1c34e8712e0906"]
 tags = {
 Name = var.instance_names[count.index]
